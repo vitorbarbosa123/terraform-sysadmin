a2c6e820a9a9ee1a5c851d3cd0c01a4fb2428d46
vitor barbosa <vitorbarbosa789@gmail.com>
2025-04-07 22:35:52 -0300

    fix: analisys -> analysis

--- feeling_analysis/api.py
+++ feeling_analysis/api.py
@@ -12,11 +12,11 @@ def sentimento_v1(frase):
   polaridade = TextBlob(frase).sentiment.polarity
   return "polaridade: {}".format(polaridade)
 
-@app.route('/sentimento/v2/<frase>')
-def sentimento_v2(frase):
-  translator = Translator()
-  en = translator.translate(frase, dest='en')
-  polaridade = TextBlob(en.text).sentiment.polarity
-  return "polaridade: {}".format(polaridade)
+# @app.route('/sentimento/v2/<frase>')
+# def sentimento_v2(frase):
+#   translator = Translator()
+#   en = translator.translate(frase, dest='en')
+#   polaridade = TextBlob(en.text).sentiment.polarity
+#   return "polaridade: {}".format(polaridade)
 
 app.run(debug=True, host='0.0.0.0', port=5000)
